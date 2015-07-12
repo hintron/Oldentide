4d222b2f3dacdf7eafcdbbe8867aa0150ca6ac04
Joseph DeVictoria <jldevictoria@gmail.com>
2015-07-12 12:07:28 -0700

    Moving to 4.8.1

--- Source/Oldentide/Oldentide.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "Oldentide.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Oldentide, "Oldentide" );
- 
--- Source/Oldentide/Oldentide.h
+++
@@ -1,8 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#ifndef __OLDENTIDE_H__
-#define __OLDENTIDE_H__
-
-#include "EngineMinimal.h"
-
-#endif
