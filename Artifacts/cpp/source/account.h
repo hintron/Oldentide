840f35f27e90d853d6b3f0d50edf51f159a74144
Joseph DeVictoria <jldevictoria@gmail.com>
2015-07-12 10:46:20 -0700

    Fixed naming convention.

---
+++ Artifacts/cpp/source/account.h
@@ -0,0 +1,9 @@
+// This class will provide account information to the game.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <set>
+#include <cctype>
+#include "pfunctions.h"
+using namespace std;
