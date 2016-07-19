1907cafdec6b2bed6d02fbafb8f60493c7529e0f
Michael Hinton <github@hinton.e4ward.com>
2016-07-19 15:01:12 -0600

    PassGen: Can now generate key based off passed salt
    Created a second argument, so user can pass in salt in hex
    and generate the key for the given password.
    If second argument is null, then it generates random salt.
    Cleaned up comments and code structure.
    Removed unecessary hex string allocation.

--- Server/PasswordGenerator.cpp
+++ Server/PasswordGenerator.cpp
@@ -7,37 +7,54 @@
 #include <openssl/evp.h>
 #include <string.h>
 #include <openssl/bn.h>
-// // TODO: Use uint8_t in place of unsigned char from stdtype?
-// // #include <stdtype.h>
-// // #include <assert.h>
 
-// Terminology:
+//
+//// Terminology:
+//
 // message = plaintext
 // message digest function = hash function
 // message digest (MD) = digest = fingerprint = output of a hash function
 
+//
+//// Task:
+//
 // Create a program that will take an input password,
 // generate a random salt, stretch the password for n iterations,
 // save the salted password and salt in the sqlite db, and 
 // time the process to see how long it took, sending an error if too quick (< 200ms)
 
-#define MAX_PASSWORD_LENGTH 64
 #define MIN_PASSWORD_LENGTH 8
+#define MAX_PASSWORD_LENGTH 64
 #define SALT_BIT_SIZE 512
+// Iterations should be calibrated so the whole process takes 200-1000 ms
+// If it is any quicker, it defeats the point of salting and stretching!
+// 2^20 iterations will effectively add 20 bits to the password
+const long long ITERATIONS = 1 << 20;
 
-// TODO: Create some simple test cases to prove that I'm doing it the way I think I am
+//
+//// TESTING:
+//
+// Run this: 
+//      ./passwordgen.o [password]
+// This will generate a key using a randomly-created salt and the password.
+// Copy the 512-bit salt hex string and paste it into this command using the same password:
+//      ./passwordgen.o [password] [paste_salt_here]
+// If the output keys for both commands are the same, then it worked properly!
+
+
 // TODO: Modularize code for reuse, so creating a new key uses same code for return visits
 // TODO: Time code and print that to output
 main(int argc, char *argv[]) {
     EVP_MD_CTX *md_context;
     const EVP_MD *md_function;
-    // TODO: I don't think I need a temp string
+    // TODO: I don't think I need a temp string. Need to test
+    // NOTE: EVP_MAX_MD_SIZE is 64 (512 bits)
     unsigned char md_value[EVP_MAX_MD_SIZE];
     unsigned char md_value_temp[EVP_MAX_MD_SIZE];
     unsigned int md_len, i;
 
     if(!argv[1]) {
-        printf("Usage: ./passwordgen.o password-to-salt\n");
+        printf("Usage: ./passwordgen.o [password_to_salt] [salt_in_hex]\n");
         exit(1);
     }
 
@@ -57,32 +74,37 @@ main(int argc, char *argv[]) {
     }
     
 
-    printf("Salting and stretching the password...\n");
+    printf("\n\nSalting and stretching the password...\n");
 
-    // TODO: allow the option to choose between 256 and 512?
     printf("Hash Function: SHA-512\n");
     md_function = EVP_sha512();
 
     printf("Password: \"%s\"\n", password);
 
-    // TODO: Have a second param that is the salt, so that the key can be regenerated if the salt is known
-    // If I can consistently regenerate the key from the password and salt, then it works
-    
     BIGNUM * salt = BN_new();
-    // Create the random number (openssl should auto-seed from /dev/urandom)
-    BN_rand(salt, SALT_BIT_SIZE, -1, 0);
-   
+    if(!argv[2]) {
+        // Since argument was not supplied, generate a random salt
+        // Create the random number (openssl should auto-seed from /dev/urandom)
+        BN_rand(salt, SALT_BIT_SIZE, -1, 0);
+    }
+    else {
+        // Have the second param be the salt (in hex), so that the key
+        // can be manually regenerated if the salt is known
+        // convert salt argument from hex to bn
+        BN_hex2bn(&salt, argv[2]);     
+    }
+
     int salt_bytes = BN_num_bytes(salt);
     int salt_bits = BN_num_bits(salt);
     unsigned char salt_string_bin[salt_bytes];
-    // Convert the salt to other formats 
-    //unsigned char * salt_string_bin;
     int salt_length = BN_bn2bin(salt, salt_string_bin);
-    char * salt_string_hex = BN_bn2hex(salt);
     
     printf("salt length : bytes : bits\n%d : %d : %d\n", salt_length, salt_bytes, salt_bits);
-    printf("Salt (hex):\n%s\n", salt_string_hex);
-
+    printf("Salt (hex):\n");
+    for(int i = 0; i < salt_length; i++){
+        printf("%02x", salt_string_bin[i]);
+    }
+    printf("\n");
     // Run through the stretching algorithm
     
     // md_temp_value needs to be initialized to all zeros, since it's on the stack
@@ -90,10 +112,6 @@ main(int argc, char *argv[]) {
         md_value_temp[i] = 0;
     } 
 
-    // Iterations should be calibrated so the whole process takes 200-1000 ms
-    #define EXTRA_BITS_OF_SECURITY 20
-    long long ITERATIONS = 1 << EXTRA_BITS_OF_SECURITY; // i.e. 2^20
-    //long long ITERATIONS = 2;
     printf("Executing %d iterations...\n", ITERATIONS);
     for (long long i = 0; i < ITERATIONS; ++i) {
         //printf("%dth iteration: \n", i);
@@ -110,24 +128,16 @@ main(int argc, char *argv[]) {
         EVP_MD_CTX_destroy(md_context);
         // NOTE: EVP_DigestFinal() should == EVP_DigestFinal_ex() + EVP_MD_CTX_destroy()
 
-        //if(i == 0){
-        //    printf("Hash of 0th iteration: \n");
-        //    for(int j = 0; j < md_len; j++){
-        //        printf("%02x", md_value[j]);
-        //    }
-        //    printf("\n");
-        //}
-
         // Copy md_value to md_value_temp, since md_value is supposed to be a const char *
-        for(int i=0; i < EVP_MAX_MD_SIZE; ++i){
-            md_value_temp[i] = md_value[i];
+        for(int j = 0; j < EVP_MAX_MD_SIZE; ++j){
+            md_value_temp[j] = md_value[j];
         }
 
     }
     // End loop
 
     printf("Final salted and stretched password/key is: \n");
-    for(i = 0; i < md_len; i++){
+    for(int i = 0; i < md_len; i++){
         printf("%02x", md_value[i]);
     }
     printf("\n");
@@ -140,10 +150,6 @@ main(int argc, char *argv[]) {
     //// Free up memory allocations
     //
 
-    // Free the salt hex string
-    // NOTE: This function doesn't show up in the 
-    // 1.0.2 docs (shows up in master), but it works
-    OPENSSL_free(salt_string_hex);
     // Clear the allocated BIGNUM pointer
     BN_clear_free(salt);
 
@@ -165,7 +171,7 @@ x = 0
 // even if the attacker guesses the same password (e.g. "password") for each user.
 salt = rand256()
 
-for (int i = 0; i < ITERATIONS; ++i) {
+for (i = 0; i < ITERATIONS; ++i) {
     // (note: || means append)
     x = hash512(x || password || salt)
 }
