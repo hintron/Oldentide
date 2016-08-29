28820e944d9e5a151db72ae4dbada2ec811f2e45
Joseph DeVictoria <jldevictoria@gmail.com>
2016-08-28 20:09:08 -0700

    Adding changes from client end.

--- Assets/Scripts/NetworkInterface.cs
+++ Assets/Scripts/NetworkInterface.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using Oldentide.Networking;
 
-public class NetworkInterface:MonoBehaviour {
+public class NetworkInterface : MonoBehaviour {
 
 	public int maxConnections = 100;
 	public string ipaddress = "127.0.0.1"; //"73.235.106.150";
 	public int socketPort = 1337;
+	public byte packetsSent = 0x00;
+	public byte error;
 	int myUnreliableChannelId;
 	int connectionId;
 	int hostId;
-	byte packetsSent = 0x00;
-	byte error;
 
 	// Use this for initialization
 	void Start(){
@@ -20,37 +26,16 @@ public class NetworkInterface:MonoBehaviour {
 		myUnreliableChannelId = config.AddChannel(QosType.Unreliable);
 		HostTopology topology = new HostTopology(config, maxConnections);
 		hostId = NetworkTransport.AddHost(topology, socketPort);
+		Debug.Log("Pre connect connectionId: " + connectionId);
 		connectionId = NetworkTransport.Connect(hostId, ipaddress, 1337, 0, out error);
+		Debug.Log("Post connect connectionId: " + connectionId);
 		CheckErrorCode();
 	}
 
 	// Update is called once per frame
 	void Update(){
-		int recHostId; 
-	    int newConnectionId; 
-	    int newChannelId; 
-	    byte[] recBuffer = new byte[1024]; 
-	    int bufferSize = 1024;
-	    int dataSize;
-	    NetworkEventType recData = NetworkTransport.Receive(out recHostId, out newConnectionId, out newChannelId, recBuffer, bufferSize, out dataSize, out error);
-	    switch (recData)
-	    {
-	        case NetworkEventType.Nothing:         //1
-	            break;
-	        case NetworkEventType.ConnectEvent:    //2
-	        	Debug.Log("Connection packet came in!");
-	            break;
-	        case NetworkEventType.DataEvent:       //3
-	            break;
-	        case NetworkEventType.DisconnectEvent: //4
-	            break;
-	    }
-	    Debug.Log(recBuffer[0]);
-		//Debug.Log("Creating packet number: " + packetsSent);
-		byte[] message = { 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, packetsSent, 0x00, 0x00, 0x00, 0x00};
-		packetsSent++;
-		NetworkTransport.Send(hostId, connectionId, myUnreliableChannelId, message, 12, out error);
-		CheckErrorCode();
+		//sendPacket();
+		receivePacket();
 	}
 
 	// Update log file with error code...
@@ -93,5 +78,53 @@ public class NetworkInterface:MonoBehaviour {
 				Debug.Log("Error code says DNSFailure");
 				break;
 		}
+	}
+
+	/*void sendPacket(){
+		//Debug.Log("Creating packet number: " + packetsSent);
+		byte[] payload = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
+		Packet toSend = new Packet(PTYPE.CONNECT, packetsSent, 0, payload);
+		packetsSent++;
+		byte[] buffer = new byte[1024];
+		Stream stream = new MemoryStream(toSend);
+		IFormatter formatter = new BinaryFormatter();
+		formatter.Serialize(stream, toSend);
+		NetworkTransport.Send(hostId, connectionId, myUnreliableChannelId, buffer, 1024, out error);
+		CheckErrorCode();
+	}*/
+
+	void receivePacket(){
+		int recHostId; 
+	    int newConnectionId; 
+	    int newChannelId; 
+	    byte[] recBuffer = new byte[1024]; 
+	    int bufferSize = 1024;
+	    int dataSize;
+	    /*
+	    NetworkEventType recEvent = NetworkTransport.Receive(out recHostId, out newConnectionId, out newChannelId, recBuffer, bufferSize, out dataSize, out error);
+	    switch (recEvent)
+	    {
+	        case NetworkEventType.Nothing:         //1
+	        	Debug.Log("A Nothing packet came in!");
+	            break;
+	        case NetworkEventType.ConnectEvent:    //2
+	        	Debug.Log("Connection packet came in!");
+	            break;
+	        case NetworkEventType.DataEvent:       //3
+	            break;
+	        case NetworkEventType.DisconnectEvent: //4
+	            break;
+	        case NetworkEventType.BroadcastEvent: //5
+	        	break;
+	    }
+	    Debug.Log("Recieved Host ID: " + recHostId);
+	    Debug.Log("New Connection ID: " + newConnectionId);
+	    Debug.Log("New Channel ID: " + newChannelId);
+	    Debug.Log("Rec Buffer contents:");
+	    for (int i = 0; i < 10; i++){
+	    	Debug.Log(recBuffer[i]);
+	    }
+	    Debug.Log("Data size: " + dataSize);
+	    */
 	}
 }
---
+++ Assets/Scripts/Packet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Oldentide.Networking{
+
+	public enum PTYPE {
+		GENERIC,
+		ACK,
+		CONNECT,
+		DISCONNECT,
+	    GETSALT,
+	    CREATEACCOUNT,
+	    LOGIN,
+	    LISTCHARACTERS,
+	    SELECTCHARACTER,
+	    DELETECHARACTER,
+	    CREATECHARACTER,
+	    INITIALIZEGAME,
+	    UPDATEPC,
+	    UPDATENPC,
+	    SENDPLAYERCOMMAND,
+	    SENDPLAYERACTION,
+	    SENDSERVERACTION,
+	    MESSAGE,
+	    GETLATESTMESSAGE,
+	}
+
+	[Serializable]
+	public class Packet {
+		
+		public Oldentide.Networking.PTYPE packetType;
+		public int packetId;
+		public int sessionId;
+		public byte[] dummy;
+		public Packet(Oldentide.Networking.PTYPE p, int pi, int si, byte[] d){
+			packetType = p;
+			packetId = pi;
+			sessionId = si;
+			dummy = d;
+		}
+	}
+}
