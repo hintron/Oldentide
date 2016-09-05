673486245bbe8a3e69c443e0293010d93472d193
Joseph DeVictoria <jldevictoria@gmail.com>
2016-09-04 22:29:39 -0700

    Working on User interface. Character Menu Asset.

---
+++ Assets/Interface/CharacterMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMenu : MonoBehaviour {
+
+	public GUISkin characterMenuSkin;
+
+	// IMGUI interface initialization.
+	void OnGUI() {
+		GUI.skin = characterMenuSkin;
+		GUI.Window(0, new Rect(0, 0, 300, 600), ToDrag, "");
+	}
+
+	void ToDrag(int windowId) {
+		GUI.DragWindow(new Rect(0, 0, 10000, 20));
+	}
+	
+}
--- Assets/Scripts/NetworkInterface.cs
+++ Assets/Scripts/NetworkInterface.cs
@@ -47,10 +47,10 @@ public class NetworkInterface : MonoBehaviour {
 		//byte[] toSend = new byte[] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13};
 		clientSocket.SendTo(p.Serialize(), serverEndPoint);
 		//Debug.Log("Data sent: " + toSend.ToString());
-		byte[] toReceive = new byte[20];
-		IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-    	EndPoint senderRemote = (EndPoint)sender;
-		clientSocket.ReceiveFrom(toReceive, ref senderRemote);
+		//byte[] toReceive = new byte[20];
+		//IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+    	//EndPoint senderRemote = (EndPoint)sender;
+		//clientSocket.ReceiveFrom(toReceive, ref senderRemote);
 		/*int i = 0;
 		foreach (byte next in toReceive) {
 			Debug.Log("Data received @[" + i + "]: " + next);
