d766a4c3a95dd865184a852d102ec733fd94c649
Joseph DeVictoria <jldevictoria@gmail.com>
2016-12-06 21:11:52 -0800

    Working on Player Controller Script.

---
+++ Assets/Scripts/Character/OldentidePlayerCamera.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class OldentidePlayerCamera : MonoBehaviour {
+
+	public Transform target;
+	public float lookSmooth = 0.09f;
+	public Vector3 offsetFromTarget = new Vector3(0, 6, -8);
+	public float xTilt = 10;
+	public float maxZoom = -12;
+
+	Vector3 destination = Vector3.zero;
+	OldentidePlayerController playerController;
+	float rotateVel = 0;
+	float zoomInput;
+
+	void Start () {
+		SetCameraTarget(target);
+	}
+	
+	void SetCameraTarget(Transform t) {
+		target = t;
+		if (target != null) {
+			if (target.GetComponent<OldentidePlayerController>()) {
+				playerController = target.GetComponent<OldentidePlayerController>();
+			}
+			else {
+				Debug.LogError("The camera's target needs to have the Oldentide Player Controller.");
+			}
+		}
+		else {
+			Debug.LogError("Your camera needs a target.");
+		}
+	}
+
+	void Update() {
+		zoomInput = Input.GetAxis("Mouse ScrollWheel");
+		if ((offsetFromTarget.z + zoomInput) > maxZoom && (offsetFromTarget.z + zoomInput) < -2){
+			offsetFromTarget.z += zoomInput;
+		}
+	}
+
+	void LateUpdate() {
+		MoveToTarget();
+		LookAtTarget();
+	}
+
+	void MoveToTarget() {
+		destination = playerController.TargetRotation * offsetFromTarget;
+		destination += target.position;
+		transform.position = destination;
+	}
+
+	void LookAtTarget() {
+		float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+		transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+	}
+
+}
---
+++ Assets/Scripts/Character/OldentidePlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class OldentidePlayerController : MonoBehaviour {
+
+	public float inputDelay = 0.1f;
+	public float forwardVel = 12;
+	public float rotateVel = 100;
+	
+	Quaternion targetRotation;
+	Rigidbody rBody;
+	float forwardInput;
+	float turnInput;	
+
+	public Quaternion TargetRotation {
+		get { return targetRotation; }
+	}
+
+	void Start() {
+		targetRotation = transform.rotation;
+		if (GetComponent<Rigidbody>()) {
+			rBody = GetComponent<Rigidbody>();
+		}
+		else {
+			Debug.LogError("The Oldentide Player Controller's target needs a Rigidbody.");
+		}
+		forwardInput = turnInput = 0;
+	}
+
+	void GetInput() {
+		forwardInput = Input.GetAxis("Vertical");
+		turnInput = Input.GetAxis("Horizontal");
+	}
+
+	void Update() {
+		GetInput();
+		Turn();
+	}
+
+	void FixedUpdate() {
+		Run();
+	}
+
+	void Run() {
+		if (Mathf.Abs(forwardInput) > inputDelay) {
+			rBody.velocity = transform.forward * forwardInput * forwardVel;
+		}
+		else {
+			rBody.velocity = Vector3.zero;
+		}
+	}
+
+	void Turn() {
+		if (Mathf.Abs(turnInput) > inputDelay) {
+			targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+		}
+		transform.rotation = targetRotation;
+	}
+
+}
