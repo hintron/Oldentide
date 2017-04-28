c02659cb8a645914ba7396cccf463cc8f99ae107
Joseph DeVictoria <jldevictoria@gmail.com>
2017-04-27 21:22:46 -0700

    Got player login working!

---
+++ Assets/Character/OldentidePlayerInformation.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldentidePlayerInformation : MonoBehaviour {
+	public static string accountName;
+	public static string sessionId;
+}
--- Assets/Network/LoginManager.cs
+++ Assets/Network/LoginManager.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System.Text.RegularExpressions;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class LoginManager : MonoBehaviour {
+
+	public GameObject failedLogin;
+	public GameObject playerInformation;
+
+	void Start () {
+		failedLogin.SetActive(false);
+	}
 
 	public void CloseGame () {
 		Application.Quit();
@@ -16,12 +24,21 @@ public class LoginManager : MonoBehaviour {
 	}
 
 	private IEnumerator SubmitLoginForm () {
-		string url = "https://" + GameObject.Find("Server_Address_Box").GetComponent<InputField>().text + "/login";
+		string url = GameObject.Find("Server_Address_Box").GetComponent<InputField>().text;
+		if (url != "") {
+			url = "https://" + url + "/login";
+		}
+		else {
+			url = GameObject.Find("Server_Address_Box").GetComponent<InputField>().placeholder.GetComponent<Text>().text;
+			url = "https://" + url + "/login";
+		}
 		Debug.Log(url);
 		Debug.Log(GameObject.Find("Password_Box").GetComponent<InputField>().text);
 		WWWForm form = new WWWForm();
-		form.AddField( "login_username", GameObject.Find("Username_Box").GetComponent<InputField>().text );
-		form.AddField( "login_password", GameObject.Find("Password_Box").GetComponent<InputField>().text );
+		string username = GameObject.Find("Username_Box").GetComponent<InputField>().text;
+		string password = GameObject.Find("Password_Box").GetComponent<InputField>().text;
+		form.AddField( "login_username", username);
+		form.AddField( "login_password", password);
 		Debug.Log(form.ToString());
 		WWW download  = new WWW(url, form);
 		yield return download;
@@ -29,8 +46,23 @@ public class LoginManager : MonoBehaviour {
             print( "Error downloading: " + download.error );
         } else {
             Debug.Log(download.text);
+            string cookie;
+            if (download.responseHeaders.TryGetValue("SET-COOKIE", out cookie)) {
+            	Regex sessionFinder = new Regex("session_id=([^;]*)");
+            	Match sessionId = sessionFinder.Match(cookie);
+            	Debug.Log(sessionId.Groups[1].Value);
+            	failedLogin.SetActive(false);
+            	OldentidePlayerInformation.accountName = username;
+            	OldentidePlayerInformation.sessionId = sessionId.Groups[1].Value;
+            	//playerInformation.GetComponent("OldentidePlayerInformation.cs").SetAccountName(username);
+            	//playerInformation.GetComponent("OldentidePlayerInformation.cs").SetSessionId(sessionId);
+            	ChangeScene("Newcomers_Guild");
+            }
+            else {
+            	Debug.Log("User failed to login correctly to the server.");
+            	failedLogin.SetActive(true);
+            }
         }
-        ChangeScene("Newcomers_Guild");
 	}
 
 	public void ChangeScene (string newScene) {
