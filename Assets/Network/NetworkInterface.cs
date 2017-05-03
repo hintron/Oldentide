dcd39ea9b0d0bdf6d027e6dd9fb929d1a2b434e1
Michael Hinton <michael.g.hinton@gmail.com>
2017-05-03 01:05:00 -0700

    Unity Client: Got Msgpack data being sent to and from Unity to the Server!
    Sent custom headers along with msgpack data from Unity client in c#.
    Parsed msgpack packets from server and decoded data.
    Tried to pattern the NetworkInterface.cs code to resemble TestClient.
    Created a hex print helper function.
    
    Press M to send connect packet and set the session for the Unity
    client. It will then send a Unity packet to the server.
    
    TODO:
    * Adapt the rest of TestClient to the Unity Client.
    * Modify Packet.cs and try sending more complicated data structures.
    * Handle all of the packets
    * Create a better workflow that uses async stuff.

--- Assets/Network/NetworkInterface.cs
+++ Assets/Network/NetworkInterface.cs
@@ -15,6 +15,14 @@ public class NetworkInterface : MonoBehaviour {
 	IPEndPoint clientEndPoint;
 	Socket clientSocket;
 
+
+	int packetNumber = 1;
+	int session = 0;
+
+	// This is the number of bytes that the oldentide header is
+	const int HEADER_SIZE = 3;
+	const int PACKET_MAX_SIZE = 512;
+
 	// Use this for initialization
 	void Start() {
 		// Set up Server End Point for sending packets.
@@ -42,97 +50,189 @@ public class NetworkInterface : MonoBehaviour {
 		}
 	}
 
+
+
+	// For the MessagePack library, using:
+	// https://github.com/neuecc/MessagePack-CSharp
+	// instead of:
+	// https://github.com/msgpack/msgpack-cli
+
+	// To install, simply download the Unity zip from the releases page:
+	// https://github.com/neuecc/MessagePack-CSharp/releases
+	// Unzip it into a folder NOT under Oldentide/
+	// (don't want unity automatically ingesting the files just yet)
+	// In the Unity editor, click Assets -> Import Package
+	// Select the MessagePack .unitypackage file and click import.
+	// e.g. ...\MessagePack.Unity.1.2.0\MessagePack.Unity.1.2.0.unitypackage"
+	// Now all the MessagePack files are loaded into the project!
+	// To use it in code, put "using MessagePack" at the top of any C# files
+
+
 	// Update is called once per frame
 	void Update() {
 
-		if(Input.GetKeyDown(KeyCode.S)){
-			Debug.Log("Sending a single packet!");
-			Debug.Log("Key Code:" + KeyCode.S);
-			PACKET_UNITY pp;
-			pp.packetType = Oldentide.Networking.PTYPE.UNITY;
-			pp.data1 = 0xde;
-			pp.data2 = 0xad;
-			pp.data3 = 0xbe;
-			pp.data4 = 0xef;
-			pp.data5 = 255;
-			byte[] p = StructureToByteArray(pp);
-			//Packet p = new Packet(Oldentide.Networking.PTYPE.UNITY, 15, 25);
-			//byte[] toSend = new byte[] {0x12, 0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13};
-			clientSocket.SendTo(p, serverEndPoint);
-			Debug.Log("Data sent: " + p.ToString());
-			/*byte[] toReceive = new byte[20];
-			IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-	    	EndPoint senderRemote = (EndPoint)sender;
-			clientSocket.ReceiveFrom(toReceive, ref senderRemote);
-			int i = 0;
-			foreach (byte next in toReceive) {
-				Debug.Log("Data received @[" + i + "]: " + next);
-				i++;
-			}*/
+		if(Input.GetKeyDown(KeyCode.M)){
+			Debug.Log("Sending a single CONNECT packet via message pack!!");
 
+			PACKET_CONNECT pp;
+			pp.sessionId = session;
+			pp.packetId = packetNumber;
+			packetNumber++;
+
+			byte [] msgpackData = MessagePackSerializer.Serialize(pp);
+			SendDataTo(clientSocket, serverEndPoint, Oldentide.Networking.PTYPE.CONNECT, msgpackData);
+
+			// Wait for the response
+			byte[] receivedMsgpackData;
+			Oldentide.Networking.PTYPE packetType = ReceiveDataFrom(out receivedMsgpackData);
+
+			Debug.Log("Server responded with packet " + packetType);
+
+
+			// TODO: What to do with this switch statement? Put it into a function of its own? Put it in a while loop? Use async stuff so it doesn't block the update function?
+
+			switch(packetType){
+				case Oldentide.Networking.PTYPE.CONNECT:
+					ConnectHandler(receivedMsgpackData);
+					break;
+				case Oldentide.Networking.PTYPE.UNITY:
+					UnityHandler(receivedMsgpackData);
+					break;
+				default:
+					Debug.Log("Unknown packet received from the server!!! " + packetType);
+					break;
+			}
+
+	   }
+
+	}
+
+
+	////
+	/// Packet handlers
+	//
+
+	void ConnectHandler(byte [] msgpackData) {
+		var data = MessagePackSerializer.Deserialize<PACKET_CONNECT>(msgpackData);
+		Debug.Log("Connect handler! sessionId: " + data.sessionId + " ; packetId: " + data.packetId);
+
+		if(data.sessionId != session){
+			Debug.Log("Setting new session to " + data.sessionId);
+			session = data.sessionId;
+		}
+		else {
+			Debug.Log("Session is already set to " + session);
 		}
 
-		if(Input.GetKeyDown(KeyCode.M)){
-			Debug.Log("Sending a single packet via message pack!!");
-			PACKET_UNITY pp;
-			pp.packetType = Oldentide.Networking.PTYPE.UNITY;
-			pp.data1 = 1;
-			pp.data2 = 8;
-			pp.data3 = 16;
-			pp.data4 = 64;
-			pp.data5 = 255;
+		// Send a Unity packet, just for kicks
+		PACKET_UNITY pp;
+		// pp.packetType = Oldentide.Networking.PTYPE.UNITY;
+		pp.sessionId = data.sessionId;
+		pp.packetId = packetNumber;
+		packetNumber++;
+		pp.data1 = 1;
+		pp.data2 = 8;
+		pp.data3 = 16;
+		pp.data4 = 64;
+		pp.data5 = 255;
 
-			// For the MessagePack library, using:
-			// https://github.com/neuecc/MessagePack-CSharp
-			// instead of:
-			// https://github.com/msgpack/msgpack-cli
+		byte [] msgpackDataToSendUnity = MessagePackSerializer.Serialize(pp);
+		SendDataTo(clientSocket, serverEndPoint, Oldentide.Networking.PTYPE.UNITY, msgpackDataToSendUnity);
 
-			// To install, simply download the Unity zip from the releases page:
-			// https://github.com/neuecc/MessagePack-CSharp/releases
-			// Unzip it into a folder NOT under Oldentide/
-			// (don't want unity automatically ingesting the files just yet)
-			// In the Unity editor, click Assets -> Import Package
-			// Select the MessagePack .unitypackage file and click import.
-			// e.g. ...\MessagePack.Unity.1.2.0\MessagePack.Unity.1.2.0.unitypackage"
-			// Now all the MessagePack files are loaded into the project!
-			// To use it in code, put "using MessagePack" at the top of any C# files
+		// // Wait for a response
+		// byte[] receivedMsgpackDataUnity;
+		// Oldentide.Networking.PTYPE packetType = ReceiveDataFrom(out receivedMsgpackDataUnity);
 
+		// if(packetType == Oldentide.Networking.PTYPE.UNITY){
+		// 	UnityHandler(receivedMsgpackDataUnity);
+		// }
+		// else {
+		// 	Debug.Log("Unknown packet received instead of unity packet...: " + packetType);
+		// }
 
-
-			// Message Pack example
-			// See https://github.com/neuecc/MessagePack-CSharp#quick-start
-
-			// call Serialize/Deserialize, that's all.
-	        var bytes = MessagePackSerializer.Serialize(pp);
-
-			clientSocket.SendTo(bytes, serverEndPoint);
-			Debug.Log("Data sent: " + bytes.ToString());
-	        // you can dump msgpack binary to human readable json.
-	        // In default, MessagePack for C# reduce property name information.
-	        // [99,"hoge","huga"]
-	        var json = MessagePackSerializer.ToJson(bytes);
-			Debug.Log("MessagePack json: " + json);
-	        var mc2 = MessagePackSerializer.Deserialize<PACKET_UNITY>(bytes);
-       }
 
 	}
 
-	public byte [] StructureToByteArray(object obj){
-    	int len = Marshal.SizeOf(obj);
-    	byte [] arr = new byte[len];
-    	IntPtr ptr = Marshal.AllocHGlobal(len);
-    	Marshal.StructureToPtr(obj, ptr, true);
-    	Marshal.Copy(ptr, arr, 0, len);
-    	Marshal.FreeHGlobal(ptr);
-    	return arr;
+
+
+	void UnityHandler(byte [] msgpackData) {
+		var data = MessagePackSerializer.Deserialize<PACKET_UNITY>(msgpackData);
+		Debug.Log("Unity handler! Data1: " + data.data1 + "; Data2: " + data.data2 + "; data3: " + data.data3 + ";");
 	}
 
-	public void ByteArrayToStructure(byte [] bytearray, ref object obj){
-    	int len = Marshal.SizeOf(obj);
-		IntPtr i = Marshal.AllocHGlobal(len);
-    	Marshal.Copy(bytearray,0, i,len);
-    	obj = Marshal.PtrToStructure(i, obj.GetType());
-    	Marshal.FreeHGlobal(i);
+
+	////
+	/// Util functions
+	//
+
+	void SendDataTo(Socket clientSocket, IPEndPoint serverEndPoint, Oldentide.Networking.PTYPE packetType, byte [] msgpackData){
+		byte[] packetToSend = new byte[msgpackData.Length + HEADER_SIZE];
+
+		// Prepend header data
+		packetToSend[0] = (byte) packetType;
+		// Convert msgpack length to a byte array
+		byte[] msgpackLength = BitConverter.GetBytes(msgpackData.Length);
+		// Copy the 2 bytes of the msgpack length into the packet at location 1
+		Array.Copy(msgpackLength, 0, packetToSend, 1, 2);
+		// Copy the msgpack data into the packet
+		Array.Copy(msgpackData, 0, packetToSend, HEADER_SIZE, msgpackData.Length);
+
+		Debug.Log("Sending packet data: ");
+		PrintHexString(packetToSend);
+		clientSocket.SendTo(packetToSend, serverEndPoint);
 	}
+
+
+	// TODO: Is there any way to receive from only the server, and not just anybody?
+	Oldentide.Networking.PTYPE ReceiveDataFrom(out byte[] data){
+		byte[] packetToReceive = new byte[PACKET_MAX_SIZE];
+		IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+		EndPoint senderRemote = (EndPoint)sender;
+		clientSocket.ReceiveFrom(packetToReceive, ref senderRemote);
+
+
+		// Get the packet type
+		Oldentide.Networking.PTYPE packetType = (Oldentide.Networking.PTYPE) packetToReceive[0];
+		// Get the length of the msgpack data
+		ushort msgpackLength = BitConverter.ToUInt16(packetToReceive, 1);
+		// Copy the msgpack data into the packet
+		data = new byte[msgpackLength];
+		Array.Copy(packetToReceive, HEADER_SIZE, data, 0, msgpackLength);
+
+		Debug.Log("Receiving msgpack data: ");
+		PrintHexString(data);
+
+		return packetType;
+	}
+
+	public void PrintHexString(byte [] bytearray){
+		string hexstring = "0x";
+		int len = bytearray.Length;
+		for (int i = 0; i < len; i++) {
+			hexstring += String.Format("{0:X2}", bytearray[i]);
+		}
+		Debug.Log(hexstring);
+	}
+
+	// public byte [] StructureToByteArray(object obj){
+	// 	int len = Marshal.SizeOf(obj);
+	// 	byte [] arr = new byte[len];
+	// 	IntPtr ptr = Marshal.AllocHGlobal(len);
+	// 	Marshal.StructureToPtr(obj, ptr, true);
+	// 	Marshal.Copy(ptr, arr, 0, len);
+	// 	Marshal.FreeHGlobal(ptr);
+	// 	return arr;
+	// }
+
+
+	// public void ByteArrayToStructure(byte [] bytearray, ref object obj){
+	// 	int len = Marshal.SizeOf(obj);
+	// 	IntPtr i = Marshal.AllocHGlobal(len);
+	// 	Marshal.Copy(bytearray,0, i,len);
+	// 	obj = Marshal.PtrToStructure(i, obj.GetType());
+	// 	Marshal.FreeHGlobal(i);
+	// }
+
+
 
 }
--- Assets/Network/Packet.cs
+++ Assets/Network/Packet.cs
@@ -10,9 +10,9 @@ namespace Oldentide.Networking {
 		ACK = 2,
 		CONNECT = 3,
 		DISCONNECT = 4,
-	    GETSALT = 5,
-	    CREATEACCOUNT = 6,
-	    LOGIN = 7,
+	    // GETSALT = 5,
+	    // CREATEACCOUNT = 6,
+	    // LOGIN = 7,
 	    LISTCHARACTERS = 8,
 	    SELECTCHARACTER = 9,
 	    DELETECHARACTER = 10,
@@ -27,222 +27,189 @@ namespace Oldentide.Networking {
 	    UNITY = 19,
 	};
 
-/*
+	[MessagePackObject]
 	public unsafe struct PACKET_ACK {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
+	    [Key(0)]
+	    public int packetId;
+	    [Key(1)]
+	    public int sessionId;
 	};
 
+	[MessagePackObject]
 	public unsafe struct PACKET_CONNECT {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
+	    [Key(0)]
+	    public int packetId;
+	    [Key(1)]
+	    public int sessionId;
 	};
 
+	[MessagePackObject]
 	public unsafe struct PACKET_DISCONNECT {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
+	    [Key(0)]
+	    public int packetId;
+	    [Key(1)]
+	    public int sessionId;
 	};
 
-	public unsafe struct PACKET_GETSALT {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte account[30];
-	    // 512-bit key -> 64 bytes -> 2 bytes per byte for hex -> 128 + 1 null = 129
-	    fixed byte saltStringHex[129];
-	};
+	// public unsafe struct PACKET_LISTCHARACTERS {
+	//     public int packetId;
+	//     public int sessionId;
+	//     fixed byte byteacter0[25];
+	//     fixed byte byteacter1[25];
+	//     fixed byte byteacter2[25];
+	//     fixed byte byteacter3[25];
+	//     fixed byte byteacter4[25];
+	//     fixed byte byteacter5[25];
+	//     fixed byte byteacter6[25];
+	//     fixed byte byteacter7[25];
+	//     fixed byte byteacter8[25];
+	//     fixed byte byteacter9[25];
+	// };
 
-	public unsafe struct PACKET_CREATEACCOUNT {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte account[30];
-	    // 512-bit key -> 64 bytes -> 2 bytes per byte for hex -> 128 + 1 null = 129
-	    fixed byte saltStringHex[129];
-	    fixed byte keyStringHex[129];
-	};
+	// public unsafe struct PACKET_SELECTCHARACTER {
+	//     public int packetId;
+	//     public int sessionId;
+	//     fixed byte byteacter[25];
+	// };
 
-	public unsafe struct PACKET_LOGIN {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte account[30];
-	    // 512-bit key -> 64 bytes -> 2 bytes per byte for hex -> 128 + 1 null = 129
-	    fixed byte keyStringHex[129];
-	};
+	// public unsafe struct PACKET_DELETECHARACTER {
+	//     public int packetId;
+	//     public int sessionId;
+	//     fixed byte byteacter[25];
+	// };
 
-	public unsafe struct PACKET_LISTCHARACTERS {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte byteacter0[25];
-	    fixed byte byteacter1[25];
-	    fixed byte byteacter2[25];
-	    fixed byte byteacter3[25];
-	    fixed byte byteacter4[25];
-	    fixed byte byteacter5[25];
-	    fixed byte byteacter6[25];
-	    fixed byte byteacter7[25];
-	    fixed byte byteacter8[25];
-	    fixed byte byteacter9[25];
-	};
+	// public unsafe struct PACKET_CREATECHARACTER {
+	//     public int packetId;
+	//     public int sessionId;
+	//     fixed byte firstName[25];
+	//     fixed byte lastName[25];
+	//     fixed byte race[25];
+	//     fixed byte gender[25];
+	//     fixed byte profession[25];
+	//     int strength;
+	//     int constitution;
+	//     int intelligence;
+	//     int dexterity;
+	//     int axe;
+	//     int dagger;
+	//     int unarmed;
+	//     int hammer;
+	//     int polearm;
+	//     int spear;
+	//     int staff;
+	//     int sword;
+	//     int archery;
+	//     int crossbow;
+	//     int sling;
+	//     int thrown;
+	//     int armor;
+	//     int dualWeapon;
+	//     int shield;
+	//     int bardic;
+	//     int conjuring;
+	//     int druidic;
+	//     int illusion;
+	//     int necromancy;
+	//     int sorcery;
+	//     int shamanic;
+	//     int spellcraft;
+	//     int summoning;
+	//     int focus;
+	//     int armorsmithing;
+	//     int tailoring;
+	//     int fletching;
+	//     int weaponsmithing;
+	//     int alchemy;
+	//     int lapidary;
+	//     int calligraphy;
+	//     int enchanting;
+	//     int herbalism;
+	//     int hunting;
+	//     int mining;
+	//     int bargaining;
+	//     int camping;
+	//     int firstAid;
+	//     int lore;
+	//     int pickLocks;
+	//     int scouting;
+	//     int search;
+	//     int stealth;
+	//     int traps;
+	//     int aeolandis;
+	//     int hieroform;
+	//     int highGundis;
+	//     int oldPraxic;
+	//     int praxic;
+	//     int runic;
+	// };
 
-	public unsafe struct PACKET_SELECTCHARACTER {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte byteacter[25];
-	};
+	// public unsafe struct PACKET_INITIALIZEGAME {
+	//     public int packetId;
+	//     public int sessionId;
+	// };
 
-	public unsafe struct PACKET_DELETECHARACTER {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte byteacter[25];
-	};
+	// public unsafe struct PACKET_UPDATEPC {
+	//     public int packetId;
+	//     public int sessionId;
+	//     fixed byte firstName[25];
+	//     fixed byte lastName[25];
+	//     fixed byte race[25];
+	//     fixed byte gender[25];
+	//     fixed byte profession[25];
+	//     int level;
+	//     int hp;
+	//     int bp;
+	//     int mp;
+	//     int ep;
+	//     int x;
+	//     int y;
+	//     int z;
+	//     float direction;
+	// };
 
-	public unsafe struct PACKET_CREATECHARACTER {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte firstName[25];
-	    fixed byte lastName[25];
-	    fixed byte race[25];
-	    fixed byte gender[25];
-	    fixed byte profession[25];
-	    int strength;
-	    int constitution;
-	    int intelligence;
-	    int dexterity;
-	    int axe;
-	    int dagger;
-	    int unarmed;
-	    int hammer;
-	    int polearm;
-	    int spear;
-	    int staff;
-	    int sword;
-	    int archery;
-	    int crossbow;
-	    int sling;
-	    int thrown;
-	    int armor;
-	    int dualWeapon;
-	    int shield;
-	    int bardic;
-	    int conjuring;
-	    int druidic;
-	    int illusion;
-	    int necromancy;
-	    int sorcery;
-	    int shamanic;
-	    int spellcraft;
-	    int summoning;
-	    int focus;
-	    int armorsmithing;
-	    int tailoring;
-	    int fletching;
-	    int weaponsmithing;
-	    int alchemy;
-	    int lapidary;
-	    int calligraphy;
-	    int enchanting;
-	    int herbalism;
-	    int hunting;
-	    int mining;
-	    int bargaining;
-	    int camping;
-	    int firstAid;
-	    int lore;
-	    int pickLocks;
-	    int scouting;
-	    int search;
-	    int stealth;
-	    int traps;
-	    int aeolandis;
-	    int hieroform;
-	    int highGundis;
-	    int oldPraxic;
-	    int praxic;
-	    int runic;
-	};
+	// public unsafe struct PACKET_UPDATENPC {
+	//     public int packetId;
+	//     public int sessionId;
+	// };
 
-	public unsafe struct PACKET_INITIALIZEGAME {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	};
+	// public unsafe struct PACKET_SENDPLAYERCOMMAND {
+	//     public int packetId;
+	//     public int sessionId;
+	//     fixed byte command[500];
+	// };
 
-	public unsafe struct PACKET_UPDATEPC {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte firstName[25];
-	    fixed byte lastName[25];
-	    fixed byte race[25];
-	    fixed byte gender[25];
-	    fixed byte profession[25];
-	    int level;
-	    int hp;
-	    int bp;
-	    int mp;
-	    int ep;
-	    int x;
-	    int y;
-	    int z;
-	    float direction;
-	};
+	// public unsafe struct PACKET_SENDSERVERCOMMAND {
+	//     public int packetId;
+	//     public int sessionId;
+	//     fixed byte command[500];
+	// };
 
-	public unsafe struct PACKET_UPDATENPC {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	};
+	// public unsafe struct PACKET_SENDPLAYERACTION {
+	//     public int packetId;
+	//     public int sessionId;
+	// };
 
-	public unsafe struct PACKET_SENDPLAYERCOMMAND {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte command[500];
-	};
-
-	public unsafe struct PACKET_SENDSERVERCOMMAND {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	    fixed byte command[500];
-	};
-
-	public unsafe struct PACKET_SENDPLAYERACTION {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	};
-
-	public unsafe struct PACKET_SENDSERVERACTION {
-	    PTYPE packetType;
-	    int packetId;
-	    int sessionId;
-	};
-*/
+	// public unsafe struct PACKET_SENDSERVERACTION {
+	//     public int packetId;
+	//     public int sessionId;
+	// };
 
 	// mark MessagePackObjectAttribute
 	[MessagePackObject]
 	public struct PACKET_UNITY {
 	    [Key(0)]
-		public PTYPE packetType;
+	    public int packetId;
 	    [Key(1)]
-		public int data1;
+	    public int sessionId;
 	    [Key(2)]
-		public int data2;
+		public int data1;
 	    [Key(3)]
-		public int data3;
+		public int data2;
 	    [Key(4)]
-		public int data4;
+		public int data3;
 	    [Key(5)]
+		public int data4;
+	    [Key(6)]
 		public int data5;
 	};
 
