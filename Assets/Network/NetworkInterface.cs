68f746f2e6f8571567cbd6d825b1a4f7a1023764
Michael Hinton <github@hinton.e4ward.com>
2017-05-05 01:16:02 -0700

    Got Unity client receiving UDP packets asynchronously
    ...but Unity will eventually crash if the Server does not respond with
    packets.

--- Assets/Network/NetworkInterface.cs
+++ Assets/Network/NetworkInterface.cs
@@ -2,6 +2,7 @@
 using Oldentide.Networking;
 using System;
 using System.IO;
+using System.Collections;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
@@ -18,6 +19,7 @@ public class NetworkInterface : MonoBehaviour {
 
 	int packetNumber = 1;
 	int session = 0;
+
 
 	// This is the number of bytes that the oldentide header is
 	const int HEADER_SIZE = 3;
@@ -48,6 +50,7 @@ public class NetworkInterface : MonoBehaviour {
 		catch (Exception e) {
 			Debug.Log("Winsock error: " + e.ToString());
 		}
+
 	}
 
 
@@ -70,95 +73,182 @@ public class NetworkInterface : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update() {
-
 		if(Input.GetKeyDown(KeyCode.M)){
-			Debug.Log("Sending a single CONNECT packet via message pack!!");
-
-			PACKET_CONNECT pp;
-			pp.sessionId = session;
-			pp.packetId = packetNumber;
-			packetNumber++;
-
-			byte [] msgpackData = MessagePackSerializer.Serialize(pp);
-			SendDataTo(clientSocket, serverEndPoint, Oldentide.Networking.PTYPE.CONNECT, msgpackData);
-
-			// Wait for the response
-			byte[] receivedMsgpackData;
-			Oldentide.Networking.PTYPE packetType = ReceiveDataFrom(out receivedMsgpackData);
-
-			Debug.Log("Server responded with packet " + packetType);
-
-
-			// TODO: What to do with this switch statement? Put it into a function of its own? Put it in a while loop? Use async stuff so it doesn't block the update function?
-
-			switch(packetType){
-				case Oldentide.Networking.PTYPE.CONNECT:
-					ConnectHandler(receivedMsgpackData);
-					break;
-				case Oldentide.Networking.PTYPE.UNITY:
-					UnityHandler(receivedMsgpackData);
-					break;
-				default:
-					Debug.Log("Unknown packet received from the server!!! " + packetType);
-					break;
-			}
-
-	   }
-
+			StartCoroutine(ConnectToServer());
+			Debug.Log("M press!");
+		}
+		// if(Input.GetKeyDown(KeyCode.L)){
+		// 	StartCoroutine(ListCharacters());
+		// 	Debug.Log("L press!");
+		// }
 	}
+
+
+	IEnumerator ConnectToServer(){
+		Debug.Log("Sending a single CONNECT packet via message pack!!");
+
+		PACKET_CONNECT pp;
+		pp.sessionId = session;
+		pp.packetId = packetNumber;
+		packetNumber++;
+
+		byte [] msgpackData = MessagePackSerializer.Serialize(pp);
+		// Synchronously send, since it's UDP
+		SendDataTo(clientSocket, serverEndPoint, Oldentide.Networking.PTYPE.CONNECT, msgpackData);
+
+		Debug.Log("Sent CONNECT packet! Receiving packet...");
+
+		// http://stackoverflow.com/questions/3356400/operator-with-events
+		// http://stackoverflow.com/questions/6866347/lambda-anonymous-function-as-a-parameter
+
+
+		// TODO: Somehow be able to pass a callback function in, so the msgpack and ReceiveFrom() code
+		// can be completely transparent and only in a single spot
+
+		SocketAsyncEventArgs e = new SocketAsyncEventArgs();
+		e.Completed += new EventHandler<SocketAsyncEventArgs>(PacketReceivedCallback); // set callback
+		e.SetBuffer(new byte[PACKET_MAX_SIZE], 0, PACKET_MAX_SIZE); // set buffer, offset, count
+		e.RemoteEndPoint = (EndPoint) new IPEndPoint(IPAddress.Any, 0); // set endpoint
+		clientSocket.ReceiveFromAsync(e);
+
+
+		// TODO: If server doesn't respond with packets, even though it doesn't block,
+		// Unity will end up freezing afterwards when you try to stop
+		Debug.Log("After ReceiveAsync");
+
+
+		// clientSocket.ReceiveFrom(packetToReceive, ref senderRemote);
+
+		// // Wait for the response
+		// byte[] receivedMsgpackData;
+		// ReceiveDataFrom(out receivedMsgpackData);
+
+		// // Oldentide.Networking.PTYPE packetType =
+		// // Debug.Log("Server responded with packet " + packetType);
+
+		// var data = MessagePackSerializer.Deserialize<PACKET_CONNECT>(receivedMsgpackData);
+		// Debug.Log("Connect packet response! sessionId: " + data.sessionId + " ; packetId: " + data.packetId);
+
+		// if(data.sessionId != session){
+		// 	Debug.Log("Setting new session to " + data.sessionId);
+		// 	session = data.sessionId;
+		// }
+		// else {
+		// 	Debug.Log("Session is already set to " + session);
+		// }
+
+		yield return null;
+	}
+
+
+
+	// TODO: There needs to be a way to pass in a callback function, so all the internal stuff is taken care of
+	void PacketReceivedCallback(object sender, SocketAsyncEventArgs e) {
+		Debug.Log("Received Callback!");
+
+		// Debug.Log(sender);
+		Debug.Log(((Socket)sender).LocalEndPoint.ToString());
+		// Debug.Log(((Socket)sender).RemoteEndPoint.ToString());
+		// Debug.Log(e);
+
+		Debug.Log("Buffer data:");
+		PrintHexString(e.Buffer);
+
+		// Get the packet type
+		Oldentide.Networking.PTYPE packetType = (Oldentide.Networking.PTYPE) e.Buffer[0];
+		// Get the length of the msgpack data
+		ushort msgpackLength = BitConverter.ToUInt16(e.Buffer, 1);
+		// Copy the msgpack data into the packet
+		byte [] data = new byte[msgpackLength];
+		Array.Copy(e.Buffer, HEADER_SIZE, data, 0, msgpackLength);
+
+		Debug.Log("Packet type: " + packetType);
+
+		Debug.Log("Receiving msgpack data: ");
+		PrintHexString(data);
+
+		// TODO: From here, how do we combat the forces of callback hell??
+	}
+
+
+
+
+	// IEnumerator ListCharacters(){
+	// 	Debug.Log("ListCharacters()!");
+		// PACKET_LISTCHARACTERS pp;
+		// pp.sessionId = session;
+		// pp.packetId = packetNumber;
+		// pp.characterArray = null;
+		// packetNumber++;
+
+		// byte [] sendMsgpackData = MessagePackSerializer.Serialize(pp);
+		// SendDataTo(clientSocket, serverEndPoint, Oldentide.Networking.PTYPE.LISTCHARACTERS, sendMsgpackData);
+
+		// // Wait for the response
+		// byte[] receivedMsgpackData;
+		// Oldentide.Networking.PTYPE packetType = ReceiveDataFrom(out receivedMsgpackData);
+		// Debug.Log("Server responded with packet " + packetType);
+		// var data = MessagePackSerializer.Deserialize<PACKET_LISTCHARACTERS>(receivedMsgpackData);
+		// Debug.Log("ListCharacter response! sessionId: " + data.sessionId + " ; packetId: " + data.packetId);
+
+		// // TODO: Print out the character list
+		// Debug.Log(data.characterArray);
+
+	// 	yield return null;
+	// }
 
 
 	////
 	/// Packet handlers
 	//
 
-	void ConnectHandler(byte [] msgpackData) {
-		var data = MessagePackSerializer.Deserialize<PACKET_CONNECT>(msgpackData);
-		Debug.Log("Connect handler! sessionId: " + data.sessionId + " ; packetId: " + data.packetId);
+	// void ConnectHandler(byte [] msgpackData) {
+	// 	var data = MessagePackSerializer.Deserialize<PACKET_CONNECT>(msgpackData);
+	// 	Debug.Log("Connect handler! sessionId: " + data.sessionId + " ; packetId: " + data.packetId);
 
-		if(data.sessionId != session){
-			Debug.Log("Setting new session to " + data.sessionId);
-			session = data.sessionId;
-		}
-		else {
-			Debug.Log("Session is already set to " + session);
-		}
+	// 	if(data.sessionId != session){
+	// 		Debug.Log("Setting new session to " + data.sessionId);
+	// 		session = data.sessionId;
+	// 	}
+	// 	else {
+	// 		Debug.Log("Session is already set to " + session);
+	// 	}
 
-		// Send a Unity packet, just for kicks
-		PACKET_UNITY pp;
-		// pp.packetType = Oldentide.Networking.PTYPE.UNITY;
-		pp.sessionId = data.sessionId;
-		pp.packetId = packetNumber;
-		packetNumber++;
-		pp.data1 = 1;
-		pp.data2 = 8;
-		pp.data3 = 16;
-		pp.data4 = 64;
-		pp.data5 = 255;
+	// 	// Send a Unity packet, just for kicks
+	// 	PACKET_UNITY pp;
+	// 	// pp.packetType = Oldentide.Networking.PTYPE.UNITY;
+	// 	pp.sessionId = data.sessionId;
+	// 	pp.packetId = packetNumber;
+	// 	packetNumber++;
+	// 	pp.data1 = 1;
+	// 	pp.data2 = 8;
+	// 	pp.data3 = 16;
+	// 	pp.data4 = 64;
+	// 	pp.data5 = 255;
 
-		byte [] msgpackDataToSendUnity = MessagePackSerializer.Serialize(pp);
-		SendDataTo(clientSocket, serverEndPoint, Oldentide.Networking.PTYPE.UNITY, msgpackDataToSendUnity);
+	// 	byte [] msgpackDataToSendUnity = MessagePackSerializer.Serialize(pp);
+	// 	SendDataTo(clientSocket, serverEndPoint, Oldentide.Networking.PTYPE.UNITY, msgpackDataToSendUnity);
 
-		// // Wait for a response
-		// byte[] receivedMsgpackDataUnity;
-		// Oldentide.Networking.PTYPE packetType = ReceiveDataFrom(out receivedMsgpackDataUnity);
+	// 	// // Wait for a response
+	// 	// byte[] receivedMsgpackDataUnity;
+	// 	// Oldentide.Networking.PTYPE packetType = ReceiveDataFrom(out receivedMsgpackDataUnity);
 
-		// if(packetType == Oldentide.Networking.PTYPE.UNITY){
-		// 	UnityHandler(receivedMsgpackDataUnity);
-		// }
-		// else {
-		// 	Debug.Log("Unknown packet received instead of unity packet...: " + packetType);
-		// }
-
-
-	}
+	// 	// if(packetType == Oldentide.Networking.PTYPE.UNITY){
+	// 	// 	UnityHandler(receivedMsgpackDataUnity);
+	// 	// }
+	// 	// else {
+	// 	// 	Debug.Log("Unknown packet received instead of unity packet...: " + packetType);
+	// 	// }
 
 
+	// }
 
-	void UnityHandler(byte [] msgpackData) {
-		var data = MessagePackSerializer.Deserialize<PACKET_UNITY>(msgpackData);
-		Debug.Log("Unity handler! Data1: " + data.data1 + "; Data2: " + data.data2 + "; data3: " + data.data3 + ";");
-	}
+
+
+	// void UnityHandler(byte [] msgpackData) {
+	// 	var data = MessagePackSerializer.Deserialize<PACKET_UNITY>(msgpackData);
+	// 	Debug.Log("Unity handler! Data1: " + data.data1 + "; Data2: " + data.data2 + "; data3: " + data.data3 + ";");
+	// }
 
 
 	////
@@ -183,8 +273,7 @@ public class NetworkInterface : MonoBehaviour {
 	}
 
 
-	// TODO: Is there any way to receive from only the server, and not just anybody?
-	Oldentide.Networking.PTYPE ReceiveDataFrom(out byte[] data){
+   Oldentide.Networking.PTYPE ReceiveDataFrom(out byte[] data){
 		byte[] packetToReceive = new byte[PACKET_MAX_SIZE];
 		IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
 		EndPoint senderRemote = (EndPoint)sender;
