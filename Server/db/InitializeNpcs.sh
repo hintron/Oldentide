0c4f4854c160641f55e83054117838b52031ad5d
Joseph DeVictoria <jldevictoria@gmail.com>
2017-09-02 12:11:57 -0700

    Added code to initialize npcs from csv file.

---
+++ Server/db/InitializeNpcs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Filename:    InitializeNpcs.sh
+# Author:      Joseph DeVictoria
+# Date:        September_2_2017
+# Purpose:     Simple script populate the NPC table in the databse with NPC information and nodes.
+
+echo "Populating NPCs for the first time..."
+
+# Make sure I am in this directory.
+cd $(dirname $0)
+
+# Removing the header line from the csv file.
+tail -n +2 "Npcs.csv" > "Npcs.init"
+
+# Import to sqlite3
+sqlite3 Oldentide.db <<EOF
+.headers on
+.mode csv
+.import Npcs.init npcs
+.quit
+EOF
+echo "NPCs Populated."
+
+# Delete temp file.
+rm "Npcs.init"
