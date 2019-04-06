585279f15db6078cf31d063f7983f1454d2f7f02
Michael Hinton <michael.g.hinton@gmail.com>
2019-04-06 00:42:37 -0600

    Comment out listener code in test_client.go
    This conflicts with the server code (port in use), and not sure what
    the point is.

--- test_client/test_client.go
+++ test_client/test_client.go
@@ -39,19 +39,19 @@ func main() {
 	fmt.Println("\n-------------------------------------------------------")
 	fmt.Println("Starting Oldentide test client!")
 	fmt.Println("-------------------------------------------------------")
-	// Listener.
-	client_address := net.UDPAddr{
-		IP:   net.IP{0, 0, 0, 0},
-		Port: sport,
-	}
-	listen_socket, err := net.ListenUDP("udp", &client_address)
-	checkErr(err)
+	// // Listener.
+	// client_address := net.UDPAddr{
+	// 	IP:   net.IP{0, 0, 0, 0},
+	// 	Port: sport,
+	// }
+	// listen_socket, err := net.ListenUDP("udp", &client_address)
+	// checkErr(err)
 	// Set up server connection.
 	// Create udp socket description struct.
 	server_connection, err := net.Dial("udp", sadd+":"+strconv.Itoa(sport))
 	checkErr(err)
 
-	defer listen_socket.Close()
+	// defer listen_socket.Close()
 	defer server_connection.Close()
 
 	p := shared.Pc{Id: 0, Accountid: 0, Firstname: "Joseph", Lastname: "DeVictoria", Guild: "Gremlins", Race: "Human", Gender: "Male", Face: "White", Skin: "White", Profession: "Engineer", Alive: true, Plevel: 51, Dp: 12000, Hp: 450, Maxhp: 450, Bp: 250, Maxbp: 250, Mp: 300, Maxmp: 300, Ep: 150, Maxep: 150, Strength: 65, Constitution: 45, Intelligence: 50, Dexterity: 50, Axe: 0, Dagger: 0, Unarmed: 499, Hammer: 0, Polearm: 0, Spear: 0, Staff: 0, Sword: 0, Archery: 0, Crossbow: 0, Sling: 0, Thrown: 0, Armor: 300, Dualweapon: 499, Shield: 0, Bardic: 0, Conjuring: 0, Druidic: 0, Illusion: 0, Necromancy: 0, Sorcery: 0, Shamanic: 0, Spellcraft: 0, Summoning: 0, Focus: 0, Armorsmithing: 0, Tailoring: 0, Fletching: 0, Weaponsmithing: 0, Alchemy: 0, Lapidary: 0, Calligraphy: 0, Enchanting: 0, Herbalism: 0, Hunting: 0, Mining: 0, Bargaining: 0, Camping: 0, Firstaid: 0, Lore: 0, Picklocks: 0, Scouting: 0, Search: 0, Stealth: 0, Traps: 0, Aeolandis: 0, Hieroform: 0, Highgundis: 0, Oldpraxic: 100, Praxic: 100, Runic: 0, Head: "None", Chest: "None", Arms: "None", Hands: "None", Legs: "None", Feet: "None", Cloak: "None", Necklace: "None", Ringone: "None", Ringtwo: "None", Righthand: "None", Lefthand: "None", Zone: "Iskirrian Plains", X: 0, Y: 0, Z: 0, Direction: 47.3}
